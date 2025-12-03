8916329e6e6a110b8520079679eb614a260b71be
Ruslan <rusrasbeer2331@gmail.com>
2025-12-03 13:40:55 +0200

    9 п

--- angular-project/src/app/features/item-form/item-form.ts
+++ angular-project/src/app/features/item-form/item-form.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgIf } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { DataService } from '../../shared/services/data.service';
 
 @Component({
@@ -11,26 +11,55 @@ import { DataService } from '../../shared/services/data.service';
   styleUrls: ['./item-form.css'],
   imports: [
     ReactiveFormsModule,
-    NgIf,
-    RouterModule
+    NgIf
   ]
 })
 export class ItemFormComponent {
 
+  showPopupError = '';
+
   constructor(private dataService: DataService, private router: Router) {}
 
   form = new FormGroup({
-    title: new FormControl('', Validators.required),
-    category: new FormControl('', Validators.required),
-    description: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    duration: new FormControl('', Validators.required),
-    price: new FormControl('', Validators.required)
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+
+    category: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2)
+    ]),
+
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10)
+    ]),
+
+    duration: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.pattern(/^[0-9]+$/)
+    ]),
+
+    price: new FormControl('', [
+      Validators.required,
+      Validators.min(0),
+      Validators.pattern(/^[0-9]+(\.[0-9]+)?$/)
+    ]),
   });
 
   submit() {
-    if (this.form.invalid) return;
+    if (this.form.invalid) {
+      this.showPopupError = 'Форма містить помилки. Перевірте введені дані!';
+      return;
+    }
 
     this.dataService.addItem(this.form.value);
     this.router.navigate(['/items']);
+  }
+
+  closePopup() {
+    this.showPopupError = '';
   }
 }
